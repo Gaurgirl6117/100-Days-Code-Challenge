e910c74fe75293174bc7b96f59d30042a97adba1
Parinati Gaur <107859829+Gaurgirl6117@users.noreply.github.com>
2023-04-16 12:48:46 +0530

    Create Sort an array of 0s, 1s and 2s.py

---
+++ Day 3/Sort an array of 0s, 1s and 2s.py
@@ -0,0 +1,12 @@
+# Github: Gaurgirl6117
+# DAY 3 of DAY 100
+# Reverse words in a given string - GeeksorGeeks
+# https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        
+        arr.sort()
+        return arr.sort
+            
