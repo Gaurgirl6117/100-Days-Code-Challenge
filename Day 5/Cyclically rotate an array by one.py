1dd7269278151af3f5453b0221cf9798aa0b02c1
Parinati Gaur <107859829+Gaurgirl6117@users.noreply.github.com>
2023-04-18 22:01:33 +0530

    Update Cyclically rotate an array by one.py

--- Day 5/Cyclically rotate an array by one.py
+++ Day 5/Cyclically rotate an array by one.py
@@ -4,12 +4,14 @@
 # https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
 
 def rotate( arr, n):
+    x = arr[n-1]
     
-    for i in range (1,n):
-        arr.insert(0, arr[-1])
-        arr.pop()
+    for i in range(0,n):
+        a = arr[i]
+        arr[i] = arr[n-1]
+        arr[n-1] = a 
         
-        return arr
+    return arr
         
     
     
