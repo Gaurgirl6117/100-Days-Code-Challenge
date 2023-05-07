2409b4b1b105b827dc072efc0d515e2e2116a2b4
Parinati Gaur <107859829+Gaurgirl6117@users.noreply.github.com>
2023-05-07 08:55:50 +0530

    Create Common elements.py

---
+++ Day 12/Common elements.py
@@ -0,0 +1,22 @@
+# Github: Gaurgirl6117
+# DAY 11 of DAY 100
+# Common elements - GeeksorGeeks
+# https://practice.geeksforgeeks.org/problems/common-elements1132/1
+
+#User function Template for python3
+
+class Solution:
+    def commonElements (self,A, B, C, n1, n2, n3):
+        # your code here
+        a = set(A)
+        b = set(B)
+        c = set(C)
+        
+        inter_ab = a.intersection(b)
+        inter_abc = inter_ab.intersection(c)
+        
+        l = list(inter_abc)
+        l.sort()
+        return l
+        
+        
